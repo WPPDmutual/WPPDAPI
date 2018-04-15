3fe678e2dbec5e51b33e4240a9677aa1240f1a66
WPPD <38340933+WPPDmutual@users.noreply.github.com>
2018-04-15 11:55:38 -0700

    Update twitter1.py

--- twitter1.py
+++ twitter1.py
@@ -4,10 +4,10 @@ import time
 import twitter
 from twitter import *
 
-consumer_key = 'bmU116990LaXs1axkdBpFG45Y'
-consumer_secret = 'rkTK9Gr6XGruDLyCyHL8Tr7y2KVMzDLRNlkngqdaw9d9nqPXhG'
-access_token = '983503114769653760-7NA0pPi3tc5b12eZKGEzTHHRdmMf48g'
-access_secret = '9OiLPusP4qOZf4MkWo41L3eKB8kPQba1RnnTn99Dni4iU'
+consumer_key = 'insert yours here'
+consumer_secret = 'insert yours here'
+access_token = 'insert yours here'
+access_secret = 'insert yours here'
 t = Twitter(auth=OAuth(access_token, access_secret, consumer_key, consumer_secret))
 data = t.search.tweets(q='#awesome') #change awesome to whatever you want to query for
 
